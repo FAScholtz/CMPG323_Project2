bd519b1091e10029047b388fde27440f27824f56
FAScholtz <fascholtz1982@gmail.com>
2020-10-15 22:11:16 +0200

    initial commit

---
+++ Dim_Dat_Pro/Dim_Dat_Pro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dim_Dat_Pro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
